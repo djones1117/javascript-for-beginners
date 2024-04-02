a49fd6c60f6badb1d6bd6f4eb5b53d57fe513de9
djones1117 <socjones21@yahoo.com>
2024-04-02 15:31:39 -0400

    string and escape characters

--- js/chapter2_variables.js
+++ js/chapter2_variables.js
@@ -1,8 +1,8 @@
-let firstName = 'Dylan';
-    firstName = 'Bob';
-    //second time you call a variable only use the name
+let firstName = "Dylan";
+firstName = "Bob";
+//second time you call a variable only use the name
 
-// variables definitions consist of 3 parts - variable defining keyword(let,var, const), 
+// variables definitions consist of 3 parts - variable defining keyword(let,var, const),
 // a name and a value
 // let and var is used when a variable might need a new value assigned to them
 //var has global scope and let has block scope. Var's global scope means that
@@ -19,3 +19,62 @@ const PI = 3.13159;
 //     PI = 4;
 // console.log(PI)
 //this will result in typeerror
+
+//Variables
+//string
+// you can use single quotes or double quotes or back ticks to delcare a string
+//single and double quotes can be used like so
+let singleString = 'hi there!';
+let doubleString = "hello there!";
+//you can use either option but be sure to be consistent
+
+// let funActivity = 'Let's learn JavaScript'
+//this will log an error as the string will end within lets
+
+let funActivity = "Let's learn Javascript";
+//this is fine because the string is looking for another double quote so this lets you use
+//single quotes as literal letters inside a string
+
+//the same can happen with double quotes for example
+// let question = "Do you want to learn JavaScript? "yes!" ";
+//this will result in an error unless you use single quotes at the yes because the compiler
+//will not distinguish the diff between double quotes in diff context and output a error
+
+/* In a string using backticks, you can point to variables and the variables value
+will be substituted into the line. In this example
+*/
+let language = "JavaScript";
+let message = `Let's learn ${language}`;
+console.log(message);
+//as you can see the language variable gets replaced with its value: Javascript
+let player = "Gavi";
+let team = "Barcelona";
+let champions = `${player} plays well for ${team} in the champions league!`;
+console.log(champions);
+
+
+//Escape Characters
+/* say we want to have double quotes, single quotes, and backticks in our string
+we would have a problem as this cannot be done with just what we have learned so far
+there is a solution to this problem tho. There is a special character that can be used
+to tell javascript "do not take the next character as you normally would"
+this is the escape character the backslash
+
+in this example below the backslash can be used to ensure your interpreter doesnt see 
+the single or double quotes marks and end the string too early
+*/
+
+let str = "Hello, what's your name? Is it \"Mike\"?";
+console.log(str);
+let str2 = 'Hello, what\'s your name? Is it "Mike"?';
+console.log(str2);
+
+//the escape character has more purpose as well
+/* with \n you can create a line break
+   you can include a backslash character with \\ 
+*/
+
+let str3 = "I'm a new \nline";
+let str4 = "I'm containing a backslash: \\!";
+console.log(str3)
+console.log(str4)
