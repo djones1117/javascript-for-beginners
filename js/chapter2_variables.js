29fbf3c1d9b3ea8059d249f17f70ea8761b4d15d
djones1117 <socjones21@yahoo.com>
2024-04-16 13:40:06 -0400

    chapter 1 and 2 summary added

--- js/chapter2_variables.js
+++ js/chapter2_variables.js
@@ -814,3 +814,14 @@ console.log(!(sumNum < sumNum2));
 
 //sumNum is smaller than sumNum2 so its true but it gets negated by the exclamation mark and logs false to the console.
 
+//Summary
+/* In this chapter we dealt with the first two programming building blocks: variables and operators. Variables are special fields that have a name and contain values. We declare
+a variable by using one of the special variable-defining keywords: let, var, or const. Variables enable us to make our scripts dynamic, store values, access them
+later, and change them later. We discussed some primitive data types, including strings, numbers, booleans, and symbols as well as more abstract types such
+as undefined and null. You learned how to determine the type of a variable using the typeof word. and you aw how you can convert data types by using 
+the built in javascript methods Number(), String(), and Boolean().Then we moved on to discuss operators. Operators enable us to work with our variables.
+They can be used to perform calculations, compare variables, and more. The operators we discussed included arithmetic operators, assignment operators, 
+comparison operators and logical operators.
+
+you are now ready to handle more complex data types such as arrays and objects.*/
+
