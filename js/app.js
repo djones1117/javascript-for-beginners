a6ab9cb58d220357918641133e1ff0d41ead46ee
djones1117 <socjones21@yahoo.com>
2024-11-22 14:41:07 -0500

    make message appear on click

--- js/app.js
+++ js/app.js
@@ -5,13 +5,25 @@ console.log('hello world')
 //this isnt neeeded if you call your script below the button though!
 document.addEventListener('DOMContentLoaded', function() {
 
-    prompt("hi there")
 
     const buttonAlert = document.getElementById("buttonAlert")
+    const loginButton = document.querySelector(".login-btn")
+    const menuTab = document.querySelector(".menu")
     
     
     buttonAlert.addEventListener("click", function (event) {
         event.stopPropagation();
         alert("you clicked the button");
     });
+
+    loginButton.addEventListener("click", function (event) {
+        event.stopPropagation();
+        toggileMenu();
+    });
+
+    function toggileMenu() {
+        menuTab.classList.toggle('show');
+    };
+
+
 });
